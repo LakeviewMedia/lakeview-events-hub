6597235465260c7645ac8bc2f9b0c05c6ac39272
LakeviewMedia <media@ourlakeviewchurch.org>
2026-09-05 11:36:25 -0400

    Update config.js

--- go/config.js
+++ go/config.js
@@ -1,3 +1,27 @@
-const weeklyLinks = {
-  youversionNotes: "http://bible.com/events/49656593"
+const trackedLinks = {
+
+  // Weekly YouVersion Notes
+  notes: {
+    destination: "http://bible.com/events/49656593",
+    event: "cc_youversion_notes_click",
+    title: "YouVersion Notes",
+    action: "sermon_notes"
+  },
+
+  // Connect Card
+  connect: {
+    destination: "https://ourlakeviewchurch.churchcenter.com/people/forms/946726",
+    event: "cc_connect_card_click",
+    title: "Connect Card",
+    action: "connect_card"
+  },
+
+  // Time Apart Registration
+  timeapart: {
+    destination: "https://brushfire.com/ohioministry/timeapart2026/628586/register",
+    event: "cc_time_apart_registration_click",
+    title: "Time Apart Registration",
+    action: "registration"
+  }
+
 };
