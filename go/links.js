cefbfc2e21d03e88feb3953c04108734eb8b4d5c
LakeviewMedia <media@ourlakeviewchurch.org>
2026-09-05 12:04:09 -0400

    Create links.js

---
+++ go/links.js
@@ -0,0 +1,19 @@
+const links = {
+  connect: {
+    name: "Connect Card",
+    url: "https://ourlakeviewchurch.churchcenter.com/people/forms/946726",
+    event: "cc_connect_card_click"
+  },
+
+  notes: {
+    name: "YouVersion Notes",
+    url: "http://bible.com/events/49656593",
+    event: "cc_youversion_notes_click"
+  },
+
+  timeapart: {
+    name: "Time Apart Registration",
+    url: "https://brushfire.com/ohioministry/timeapart2026/628586/register",
+    event: "cc_time_apart_registration_click"
+  }
+};
