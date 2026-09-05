347cfd753c832068214df55e94953402c4843e7e
LakeviewMedia <media@ourlakeviewchurch.org>
2026-09-05 12:37:58 -0400

    Update links.js

--- go/links.js
+++ go/links.js
@@ -1,4 +1,5 @@
 const links = {
+
   connect: {
     name: "Connect Card",
     url: "https://ourlakeviewchurch.churchcenter.com/people/forms/946726",
@@ -15,5 +16,23 @@ const links = {
     name: "Time Apart Registration",
     url: "https://brushfire.com/ohioministry/timeapart2026/628586/register",
     event: "cc_time_apart_registration_click"
-  }
+  },
+
+  riverchurch: {
+    name: "The River Church Event Registration",
+    url: "https://www.theriverchurch.church/events?sapurl=LyttdnBkL2xiL2V2Lys5ODRmYjVuL3JlZ2lzdGVyP2JyYW5kaW5nPXRydWUmZW1iZWQ9dHJ1ZSZyZWNlbnRSb3V0ZT1hcHAud2ViLWFwcC5saWJyYXJ5LmNhbGVuZGFyJnJlY2VudFJvdXRlU2x1Zz0lMkJwdzR0czJj",
+    event: "cc_river_church_registration_click"
+  },
+
+  isaidyes: {
+  name: "I Said YES",
+  url: "https://ourlakeviewchurch.churchcenter.com/people/forms/950846",
+  event: "cc_i_said_yes_click"
+}
+
+  jointeam: {
+  name: "Join A Team",
+  url: "https://ourlakeviewchurch.churchcenter.com/people/forms/952793",
+  event: "cc_join_a_team_click"
+}
 };
